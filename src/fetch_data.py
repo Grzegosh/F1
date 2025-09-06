b7fd4b008ecd48e5f55462a064641d62c12bf276
Grzegosh <grzegorznaporowski1@gmail.com>
2025-09-06 15:10:04 +0200

    Fixed bugs connected with amount of data

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -19,6 +19,23 @@ class DataFetcher:
         response.raise_for_status()  # Raise an error for bad responses
         data = response.json()
         return pd.DataFrame(data)
+    
+    # Funtions to fetch specific datasets
 
     def fetch_drivers(self) -> pd.DataFrame:
         return self.fetch_data(self.config.drivers_url)
+    
+    def fetch_pit_stops(self) -> pd.DataFrame:
+        return self.fetch_data(self.config.pit_url)
+    
+    def fetch_sessions(self) -> pd.DataFrame:
+        return self.fetch_data(self.config.sessions_url)
+    
+    def fetch_starting_grid(self) -> pd.DataFrame:
+        return self.fetch_data(self.config.starting_grid_url)   
+    
+    def fetch_overtakes(self) -> pd.DataFrame:
+        return self.fetch_data(self.config.overtakes_url)
+
+    def fetch_session_results(self) -> pd.DataFrame:
+        return self.fetch_data(self.config.session_results_url)
